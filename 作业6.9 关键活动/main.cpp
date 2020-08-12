e04d87fbf0704fbb762b8abcbe1acd275941b268
Cook21 <shaqray@163.com>
2020-08-12 20:34:50 +0800

    111

--- 作业6.9 关键活动/main.cpp
+++ 作业6.9 关键活动/main.cpp
@@ -22,60 +22,68 @@ public:
     vector<list<AdjTableElem>> adjacencyTable;
     vector<list<AdjTableElem>> reversedAdjacencyTable; //关键
     vector<int> inDegree;
+    vector<int> outDegree;
     vector<int> earliestCompletionTime;
     vector<int> latestCompletionTime; //关键
+    int result;
     static const int posInfinity;
     Graph(int size)
     {
         adjacencyTable = vector<list<AdjTableElem>>(size);
         reversedAdjacencyTable = vector<list<AdjTableElem>>(size); //关键
         inDegree = vector<int>(size, 0);
-        earliestCompletionTime = vector<int>(size, 0);
+        outDegree = vector<int>(size, 0);
+        
     }
     void connect(NodeID node1, NodeID node2, int price)
     {
-        adjacencyTable[node1].push_front(AdjTableElem { node2, price });
+        adjacencyTable[node1].push_front(AdjTableElem { node2, price }); //保证KeyPath输出顺序
         reversedAdjacencyTable[node2].push_front(AdjTableElem { node1, price }); //关键
         inDegree[node2]++;
+        outDegree[node1]++;
     }
     int TopSort()
     {
-        vector<NodeID> inDegreeEqualsZero {};
+        earliestCompletionTime = vector<int>(adjacencyTable.size(), posInfinity);
+        deque<NodeID> inDegreeEqualsZero {};
         for (NodeID i = 0; i < inDegree.size(); i++) {
             if (inDegree[i] == 0) {
+                earliestCompletionTime[i] = 0;
                 inDegreeEqualsZero.push_back(i);
             }
         }
+        
         while (inDegreeEqualsZero.size() != 0) {
-            NodeID currNode = inDegreeEqualsZero.back();
-            inDegreeEqualsZero.pop_back();
+            NodeID currNode = inDegreeEqualsZero.front();
+            inDegreeEqualsZero.pop_front();
             for (auto neighbour : adjacencyTable[currNode]) {
                 if (--inDegree[neighbour.nodeID] == 0) {
                     inDegreeEqualsZero.push_back(neighbour.nodeID);
                 }
-                if (earliestCompletionTime[currNode] + neighbour.cost > earliestCompletionTime[neighbour.nodeID]) {
+                if (earliestCompletionTime[currNode] + neighbour.cost > earliestCompletionTime[neighbour.nodeID] || earliestCompletionTime[neighbour.nodeID] == posInfinity) {
                     earliestCompletionTime[neighbour.nodeID] = earliestCompletionTime[currNode] + neighbour.cost;
                 }
             }
         }
-        int result = 0;
+        result = 0;
         for (auto compTime : earliestCompletionTime) {
             if (compTime > result) {
                 result = compTime;
             }
+        }
+        if (result == posInfinity) {
+            result = 0;
         }
         return result;
     }
     void calcLatestCompTime()
     {
         deque<NodeID> queue {};
-        vector<NodeID> isEnqueued(adjacencyTable.size(), false);
         latestCompletionTime = vector<NodeID>(adjacencyTable.size(), posInfinity);
-        for (NodeID i = 0; i < inDegree.size(); i++) {
-            if (adjacencyTable[i].size() == 0) {
+        for (NodeID i = 0; i < adjacencyTable.size(); i++) {
+            if (outDegree[i] == 0) { //找末端节点
                 queue.push_back(i);
-                isEnqueued[i]=true;
-                latestCompletionTime[i] = earliestCompletionTime[i];
+                latestCompletionTime[i] = result;
             }
         }
         while (!queue.empty()) {
@@ -85,9 +93,8 @@ public:
                 if (latestCompletionTime[currNode] - neighbour.cost < latestCompletionTime[neighbour.nodeID]) {
                     latestCompletionTime[neighbour.nodeID] = latestCompletionTime[currNode] - neighbour.cost;
                 }
-                if (!isEnqueued[neighbour.nodeID]) {
+                if (--outDegree[neighbour.nodeID] == 0) {
                     queue.push_back(neighbour.nodeID);
-                    isEnqueued[neighbour.nodeID] = true;
                 }
             }
         }
@@ -102,7 +109,7 @@ public:
         for (NodeID currNode = 0; currNode < adjacencyTable.size(); currNode++) {
             for (auto i : adjacencyTable[currNode]) {
                 if (latestCompletionTime[i.nodeID] - earliestCompletionTime[currNode] == i.cost) {
-                    cout << currNode + 1 << "->" << i.nodeID+1 << endl;
+                    cout << currNode + 1 << "->" << i.nodeID + 1 << endl;
                 }
             }
         }
@@ -121,11 +128,9 @@ int main()
         activityOnEdge.connect(node1 - 1, node2 - 1, cost);
     }
     int earliestCompletionTime = activityOnEdge.TopSort();
-    if (earliestCompletionTime == -1) {
-        cout << "Impossible" << endl;
-    } else {
-        cout << earliestCompletionTime << endl;
+    cout << earliestCompletionTime << endl;
+    if (earliestCompletionTime != 0) {
+        activityOnEdge.calcLatestCompTime();
+        activityOnEdge.findKeyPath();
     }
-    activityOnEdge.calcLatestCompTime();
-    activityOnEdge.findKeyPath();
 }
--- 作业7.1 排序/main.cpp
+++ 作业7.1 排序/main.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cmath>
 #include <deque>
 #include <iostream>
 #include <list>
@@ -11,42 +12,75 @@ using std::endl;
 using std::list;
 using std::vector;
 
-void popSort(vector<int>& container){
-    bool isSwaped=true;
-    for(int i=0;i<container.size()-1&&isSwaped;i++){
-        isSwaped=false;
-        for(int j=0;j<container.size()-i-1;j++){
-            if(container[j]>container[j+1]){
-                int temp=container[j];
-                container[j]=container[j+1];
-                container[j+1]=temp;
-                isSwaped=true;
+void popSort(vector<int>& container)
+{
+    bool isSwaped = true;
+    for (int i = 0; i < container.size() - 1 && isSwaped; i++) {
+        isSwaped = false;
+        for (int j = 0; j < container.size() - i - 1; j++) {
+            if (container[j] > container[j + 1]) {
+                int temp = container[j];
+                container[j] = container[j + 1];
+                container[j + 1] = temp;
+                isSwaped = true;
             }
         }
     }
 }
-void insertionSort(vector<int>& container){
-    for(int sortedSize=1;sortedSize<container.size();sortedSize++){
-        int curr=container[sortedSize];
+void insertionSort(vector<int>& container)
+{
+    for (int sortedSize = 1; sortedSize < container.size(); sortedSize++) {
+        int curr = container[sortedSize];
         int i;
-        for(i=sortedSize-1;i>=0&&container[i]>curr;i--){
-            container[i+1]=container[i];
+        for (i = sortedSize - 1; i >= 0 && container[i] > curr; i--) {
+            container[i + 1] = container[i];
         }
-        container[i+1]=curr;
+        container[i + 1] = curr;
     }
 }
-int main(){
+void shellSort(vector<int>& container)
+{
+    //Hibbard增量序列
+    for (int delta = pow(2, floor(log(container.size()) / log(2))) - 1; delta > 0; delta /= 2) {
+        cout << "delta:" << delta << endl;
+        for (int sortedSize = delta; sortedSize < container.size(); sortedSize++) {
+            int curr = container[sortedSize];
+            int i;
+            for (i = sortedSize - delta; i >= 0 && container[i] > curr; i -= delta) {
+                container[i + delta] = container[i];
+            }
+            container[i + delta] = curr;
+        }
+    }
+}
+void heapSort(vector<int>& container)
+{
+    std::make_heap(container.begin(), container.end());
+    auto itr=container.end();
+    while(itr!=container.begin()+1){
+        std::pop_heap(container.begin(),itr);
+        itr--;
+    }
+}
+int main()
+{
     int numOfNums;
     cin >> numOfNums;
-    vector<int> container{};
-    for(int i=0;i<numOfNums;i++){
+    vector<int> container {};
+    for (int i = 0; i < numOfNums; i++) {
         int temp;
         cin >> temp;
         container.push_back(temp);
     }
     //popSort(container);
-    insertionSort(container);
-    for(auto i:container){
-        cout << i << " ";
+    //insertionSort(container);
+    //shellSort(container);
+    heapSort(container);
+    for (auto i = container.begin(); i < container.end(); i++) {
+        if (i != container.end() - 1) {
+            cout << *i << " ";
+        } else {
+            cout << *i;
+        }
     }
 }
