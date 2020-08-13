9ce59b4ff351b180946bbabd2104c16ce4071ad6
Cook21 <shaqray@163.com>
2020-08-13 11:28:04 +0800

    111

--- 作业7.1 排序/main.cpp
+++ 作业7.1 排序/main.cpp
@@ -14,15 +14,15 @@ using std::vector;
 
 void popSort(vector<int>& container)
 {
-    bool isSwaped = true;
-    for (int i = 0; i < container.size() - 1 && isSwaped; i++) {
-        isSwaped = false;
-        for (int j = 0; j < container.size() - i - 1; j++) {
+    bool isSwapedThisRound = true;
+    for (int i = 0; i < container.size() - 1 && isSwapedThisRound; i++) {
+        isSwapedThisRound = false;
+        for (int j = 0; j < container.size() - 1 - i; j++) {
             if (container[j] > container[j + 1]) {
                 int temp = container[j];
                 container[j] = container[j + 1];
                 container[j + 1] = temp;
-                isSwaped = true;
+                isSwapedThisRound = true;
             }
         }
     }
@@ -41,8 +41,8 @@ void insertionSort(vector<int>& container)
 void shellSort(vector<int>& container)
 {
     //Hibbard增量序列
-    for (int delta = pow(2, floor(log(container.size()) / log(2))) - 1; delta > 0; delta /= 2) {
-        cout << "delta:" << delta << endl;
+    for (int delta = pow(2, floor(log(container.size()) / log(2))-1) - 1; delta > 0; delta = (delta-1)/2) {
+        //cout << "delta:" << delta << endl;
         for (int sortedSize = delta; sortedSize < container.size(); sortedSize++) {
             int curr = container[sortedSize];
             int i;
@@ -55,11 +55,65 @@ void shellSort(vector<int>& container)
 }
 void heapSort(vector<int>& container)
 {
-    std::make_heap(container.begin(), container.end());
-    auto itr=container.end();
-    while(itr!=container.begin()+1){
-        std::pop_heap(container.begin(),itr);
+    std::make_heap(container.begin(), container.end()); //默认是大顶堆
+    auto itr = container.end();
+    while (itr != container.begin() + 1) {
+        std::pop_heap(container.begin(), itr);
         itr--;
+    }
+}
+void merge(vector<int>& A, vector<int>& B, int leftStart, int rightStart, int rightEnd)
+{
+    int leftEnd = rightStart;
+    int BStart = leftStart;
+    while (leftStart < leftEnd && rightStart < rightEnd) {
+        if (A[leftStart] < A[rightStart]) {
+            B[BStart] = A[leftStart];
+            leftStart++;
+            BStart++;
+        } else {
+            B[BStart] = A[rightStart];
+            rightStart++;
+            BStart++;
+        }
+    }
+    while (leftStart < leftEnd) {
+        B[BStart] = A[leftStart];
+        leftStart++;
+        BStart++;
+    }
+    while (rightStart < rightEnd) {
+        B[BStart] = A[rightStart];
+        rightStart++;
+        BStart++;
+    }
+}
+void mergeSort(vector<int>& container)
+{
+    vector<int> tempContainer(container.size());
+    int sortedSize = 1;
+    bool isInContainer = true;
+    while (sortedSize < container.size()) {
+        for (int i = 0; i < container.size(); i += sortedSize * 2) {
+            int rightStart = i + sortedSize;
+            int rightEnd = rightStart + sortedSize;
+            if (rightStart > container.size()) {
+                rightStart = container.size();
+            }
+            if (rightEnd > container.size()) {
+                rightEnd = container.size();
+            }
+            if (isInContainer) {
+                merge(container, tempContainer, i, rightStart, rightEnd);
+            } else {
+                merge(tempContainer, container, i, rightStart, rightEnd);
+            }
+        }
+        isInContainer = !isInContainer;
+        sortedSize *= 2;
+    }
+    if (!isInContainer) {
+        container = tempContainer;
     }
 }
 int main()
@@ -75,7 +129,8 @@ int main()
     //popSort(container);
     //insertionSort(container);
     //shellSort(container);
-    heapSort(container);
+    //heapSort(container);
+    mergeSort(container);
     for (auto i = container.begin(); i < container.end(); i++) {
         if (i != container.end() - 1) {
             cout << *i << " ";
